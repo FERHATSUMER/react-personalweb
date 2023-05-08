61778a75d6f8beeca1ec61105c9d347c96fbb074
FERHATSUMER <sumerf39@gmail.com>
2023-05-08 23:33:07 +0300

    react-perssonalweb

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React from 'react';
 import './header.css';
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsTwitter, BsEnvelope } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div className='header-container'>
-    <img  className='logo'
-    src='https://d1hzl1rkxaqvcd.cloudfront.net/contest_entries/2099580/_600px/7325f77640c92dc603394adcdb428b5c.jpg'  alt=''/>
+      <img className='logo' src='https://d1hzl1rkxaqvcd.cloudfront.net/contest_entries/2099580/_600px/7325f77640c92dc603394adcdb428b5c.jpg' alt='' />
       <nav className='header-nav'>
         <ul>
-        <BsInstagram />
-
-            <li> <a href='https://www.instagram.com/serkandemir.2/' target='_blank'></a>
-            <h4>Instagram</h4>
-            </li>
-            <li> <a href='https://twitter.com/serkandemir04' target='_blank'>Twiter</a></li>
-            <li> <a href='mail:serkandemir@gmail.com' target='_blank'>Email</a></li>
+          <li>
+            <a href='https://www.instagram.com/serkandemir.2/' target='_blank' rel='noopener noreferrer'>
+              <BsInstagram />
+              <h4>Instagram</h4>
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/serkandemir04' target='_blank' rel='noopener noreferrer'>
+              <BsTwitter />
+              <h4>Twitter</h4>
+            </a>
+          </li>
+          <li>
+            <a href='mailto:serkandemir@gmail.com' target='_blank' rel='noopener noreferrer'>
+              <BsEnvelope />
+              <h4>Email</h4>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.facebook.com/serkandemir.2/' target='_blank' rel='noopener noreferrer'>
+              <FaFacebook />
+              <h4>Facebook</h4>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.youtube.com/user/serkandemir1' target='_blank' rel='noopener noreferrer'>
+              <AiOutlineYoutube />
+              <h4>Youtube</h4>
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
